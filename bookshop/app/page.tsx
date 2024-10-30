42da053ce31f7458b4611e4172c4dcdbc0454f2a
eljona406 <eljona.gjoka@fti.edu.al>
2024-10-30 13:05:01 +0000

    add project basic structure

---
+++ bookshop/app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to the Bookshop</h1>
+        <p>Browse our collection of books.</p>
+      </main>
+    </div>
+  );
+}
